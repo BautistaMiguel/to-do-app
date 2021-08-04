2391ebe89972998b6ba4e688b19f5fe5c86db149
Bautista Miguel <baucho3@gmail.com>
2021-08-04 12:33:27 -0300

    store

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import AddToDoForm from "../../components/toDo/AddToDoForm";
 import ToDoList from "../../components/toDo/ToDoList";
+import Counter from "../../counter";
 
 const Home = () => (
   <>
     <h1 style={{ textAlign: "center" }}>Bauchito's Proyect</h1>
-
+    <Counter />
     <AddToDoForm />
     <ToDoList />
   </>
